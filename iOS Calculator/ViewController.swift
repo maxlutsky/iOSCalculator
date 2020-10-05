7327a9e282ab2eb32890d9cab982e7def8924e53
Max <gmax115230@gmail.com>
2020-10-05 08:42:44 +0300

    added UI

--- iOS Calculator/ViewController.swift
+++ iOS Calculator/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var plusButton = CalcButton(label: "+", color: .orange)
+    
+    var equalButton = CalcButton(label: "=", color: .orange)
+    
+    var pointButton = CalcButton(label: ",", color: .darkGray)
+    
+    var zeroButton = CalcButton(label: "0", color: .darkGray)
+    
+    var oneButton = CalcButton(label: "1", color: .darkGray)
+    
+    var twoButton = CalcButton(label: "2", color: .darkGray)
+    
+    var threeButton = CalcButton(label: "3", color: .darkGray)
+    
+    var fourButton = CalcButton(label: "4", color: .darkGray)
+    
+    var fiveButton = CalcButton(label: "5", color: .darkGray)
+    
+    var sixButton = CalcButton(label: "6", color: .darkGray)
+    
+    var minusButton = CalcButton(label: "-", color: .orange)
+    
+    var sevenButton = CalcButton(label: "7", color: .darkGray)
+    
+    var eightButton = CalcButton(label: "8", color: .darkGray)
+    
+    var nineButton = CalcButton(label: "9", color: .darkGray)
+    
+    var multiplyButton = CalcButton(label: "x", color: .orange)
+    
+    var removeButton = CalcButton(label: "C", color: .gray)
+    
+    var invertButton = CalcButton(label: "+/-", color: .gray)
+    
+    var percentButton = CalcButton(label: "%", color: .gray)
+    
+    var divideButton = CalcButton(label: "/", color: .orange)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+        buttonBuilder()
     }
-
-
 }
 
